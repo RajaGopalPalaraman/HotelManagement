aa3a55956fc0591dbc76a15f1a0d01d0151ced51
raja-7011 <rajagopal.p@zohocorp.com>
2019-02-23 10:42:29 +0530

    UI bug fix

--- app/src/main/java/com/edot/hotelmanagement/RoomConfirmationActivity.java
+++ app/src/main/java/com/edot/hotelmanagement/RoomConfirmationActivity.java
@@ -29,93 +29,94 @@ public class RoomConfirmationActivity extends AppCompatActivity {
     private String clientDate;
 
     private boolean unLock = false;
-    private boolean enableBackButton = true;
+    private boolean enableBackButton = false;
+    private boolean booked = false;
 
     @Override
     protected void onStart() {
         super.onStart();
-        setContentView(R.layout.layout_loading);
-        unLock = false;
-        formDate = (HashMap<String, String>) getIntent().getSerializableExtra(DATA_MAP);
-        date = getIntent().getStringExtra(DATE);
-        clientDate = date;
-        int[] ints = DateChooserHelper.parseDate(date);
-        if (ints != null) {
-            date = ints[2] + "-" + ints[1] + "-" + ints[0];
-        }
-        else
-        {
-            Log.d(AppConstants.LOG_TAG,"Current session : "+date);
-        }
+        if (!booked) {
+            setContentView(R.layout.layout_loading);
+            unLock = false;
+            formDate = (HashMap<String, String>) getIntent().getSerializableExtra(DATA_MAP);
+            date = getIntent().getStringExtra(DATE);
+            clientDate = date;
+            int[] ints = DateChooserHelper.parseDate(date);
+            if (ints != null) {
+                date = ints[2] + "-" + ints[1] + "-" + ints[0];
+            } else {
+                Log.d(AppConstants.LOG_TAG, "Current session : " + date);
+            }
 
-        new AsyncTask<Void, Void, RoomBookingJSONHelper>() {
-            @Override
-            protected RoomBookingJSONHelper doInBackground(Void... voids) {
-                if (AppConstants.currentLoggedInUserID != null) {
-                    String url = "http://autoiot2019-20.000webhostapp.com/HotelManagement/roomLocker.php";
-                    HashMap<String, String> paramsMap = new HashMap<>();
-                    paramsMap.put(ROOM_ID, formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_NUMBER));
-                    paramsMap.put(DATE, date);
-                    paramsMap.put(USER_ID, AppConstants.currentLoggedInUserID);
+            new AsyncTask<Void, Void, RoomBookingJSONHelper>() {
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    enableBackButton = false;
+                }
 
-                    String data = NetworkHelperUtil.readData(url, paramsMap);
-                    if (data != null) {
-                        Log.d(AppConstants.LOG_TAG, "Room locking response : " + data);
-                        RoomBookingJSONHelper roomBookingJSONHelper =  new Gson().fromJson(data,
-                                RoomBookingJSONHelper.class);
-                        if (!(roomBookingJSONHelper.roomsList == null || roomBookingJSONHelper.roomsList.isEmpty()))
-                        {
-                            unLock = true;
-                            return roomBookingJSONHelper;
+                @Override
+                protected RoomBookingJSONHelper doInBackground(Void... voids) {
+                    if (AppConstants.currentLoggedInUserID != null) {
+                        String url = "http://autoiot2019-20.000webhostapp.com/HotelManagement/roomLocker.php";
+                        HashMap<String, String> paramsMap = new HashMap<>();
+                        paramsMap.put(ROOM_ID, formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_NUMBER));
+                        paramsMap.put(DATE, date);
+                        paramsMap.put(USER_ID, AppConstants.currentLoggedInUserID);
+
+                        String data = NetworkHelperUtil.readData(url, paramsMap);
+                        if (data != null) {
+                            Log.d(AppConstants.LOG_TAG, "Room locking response : " + data);
+                            RoomBookingJSONHelper roomBookingJSONHelper = new Gson().fromJson(data,
+                                    RoomBookingJSONHelper.class);
+                            if (!(roomBookingJSONHelper.roomsList == null || roomBookingJSONHelper.roomsList.isEmpty())) {
+                                unLock = true;
+                                return roomBookingJSONHelper;
+                            }
+                        }
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(RoomBookingJSONHelper roomInfo) {
+                    super.onPostExecute(roomInfo);
+                    enableBackButton = true;
+                    if (roomInfo == null) {
+                        setContentView(new LinearLayout(RoomConfirmationActivity.this));
+                        Toast.makeText(RoomConfirmationActivity.this,
+                                R.string.roomBlockingFailed, Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        HashMap<String, String> map = roomInfo.convertToHashMap().get(
+                                formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_NUMBER));
+                        if (map != null) {
+                            setContentView(R.layout.activity_room_confirmation);
+                            TextView textView = findViewById(R.id.rommConfirmationNumber);
+                            textView.setText(getResources().getString(R.string.roomNumber,
+                                    formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_NUMBER)));
+
+                            textView = findViewById(R.id.activity_room_confirmation_type_view);
+                            textView.setText(formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_TYPE));
+
+                            textView = findViewById(R.id.activity_room_confirmation_cost_view);
+                            textView.setText(map.get(RoomBookingJSONHelper.RoomInfo.ROOM_COST));
+
+                            textView = findViewById(R.id.activity_room_confirmation_floor_view);
+                            textView.setText(formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_GROUP));
+
+                            textView = findViewById(R.id.activity_room_confirmation_date_view);
+                            textView.setText(clientDate);
+                        } else {
+                            setContentView(new LinearLayout(RoomConfirmationActivity.this));
+                            Toast.makeText(RoomConfirmationActivity.this,
+                                    R.string.roomBlockingFailed, Toast.LENGTH_SHORT).show();
+                            finish();
                         }
                     }
                 }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(RoomBookingJSONHelper roomInfo) {
-                super.onPostExecute(roomInfo);
-                if (roomInfo == null)
-                {
-                    setContentView(new LinearLayout(RoomConfirmationActivity.this));
-                    Toast.makeText(RoomConfirmationActivity.this,
-                            R.string.roomBlockingFailed,Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-                else
-                {
-                    HashMap<String,String> map = roomInfo.convertToHashMap().get(
-                            formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_NUMBER));
-                    if (map != null)
-                    {
-                        setContentView(R.layout.activity_room_confirmation);
-                        TextView textView = findViewById(R.id.rommConfirmationNumber);
-                        textView.setText(getResources().getString(R.string.roomNumber,
-                                formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_NUMBER)));
-
-                        textView = findViewById(R.id.activity_room_confirmation_type_view);
-                        textView.setText(formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_TYPE));
-
-                        textView = findViewById(R.id.activity_room_confirmation_cost_view);
-                        textView.setText(map.get(RoomBookingJSONHelper.RoomInfo.ROOM_COST));
-
-                        textView = findViewById(R.id.activity_room_confirmation_floor_view);
-                        textView.setText(formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_GROUP));
-
-                        textView = findViewById(R.id.activity_room_confirmation_date_view);
-                        textView.setText(clientDate);
-                    }
-                    else
-                    {
-                        setContentView(new LinearLayout(RoomConfirmationActivity.this));
-                        Toast.makeText(RoomConfirmationActivity.this,
-                                R.string.roomBlockingFailed,Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                }
-            }
-        }.execute();
+            }.execute();
+        }
     }
 
     @Override
@@ -125,19 +126,26 @@ public class RoomConfirmationActivity extends AppCompatActivity {
         {
             new AsyncTask<Void, Void, Void>() {
                 @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    Toast.makeText(RoomConfirmationActivity.this, R.string.unLockingRoom, Toast.LENGTH_SHORT).show();
+                }
+                @Override
                 protected Void doInBackground(Void... voids) {
-                    String roomID = getIntent().getStringExtra(ROOM_ID);;
-                    String date = getIntent().getStringExtra(DATE);
                     String url = "http://autoiot2019-20.000webhostapp.com/HotelManagement/roomUnLocker.php";
                     HashMap<String, String> paramsMap = new HashMap<>();
-                    paramsMap.put(ROOM_ID, roomID);
+                    paramsMap.put(ROOM_ID, formDate.get(RoomBookingJSONHelper.RoomInfo.ROOM_NUMBER));
                     paramsMap.put(DATE, date);
                     paramsMap.put(USER_ID, AppConstants.currentLoggedInUserID);
 
                     String data = NetworkHelperUtil.readData(url, paramsMap);
                     return null;
                 }
-
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    unLock = false;
+                }
             }.execute();
         }
     }
@@ -207,6 +215,7 @@ public class RoomConfirmationActivity extends AppCompatActivity {
                 super.onPostExecute(aBoolean);
                 if (aBoolean)
                 {
+                    booked=true;
                     LinearLayout l = findViewById(R.id.activity_room_confirmation_room_info_view);
                     l.removeAllViews();
                     LayoutInflater inflater = LayoutInflater.from(RoomConfirmationActivity.this);
